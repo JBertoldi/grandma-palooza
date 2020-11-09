ee59db8c54c7cb41b272f67fd88c274e6154dc53
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-09 19:13:54 +0000

    Experience model validation created

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,10 @@
 class Experience < ApplicationRecord
-  belongs_to :user
-  belongs_to :grandmother
+  belongs_to :user, presence: true
+  belongs_to :grandmother, presence: true
 
   has_many :bookings
+
+  validates :title, :description, :price, :duration, presence: true
+  validates :description, length: { in: 100..500 }
+  validates :price, :duration, numericality: true
 end
