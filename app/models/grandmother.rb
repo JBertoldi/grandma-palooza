3cb2cbccafe17e85f53c380eacdf64cc2a184a12
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-09 19:19:42 +0000

    Grandmother model validation created

--- app/models/grandmother.rb
+++ app/models/grandmother.rb
@@ -1,5 +1,9 @@
 class Grandmother < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, presence: true
 
-  has_many :experiences
+  has_many :experiences, dependent: :destroy
+
+  validates :first_name, :last_name, :age, :nationality, presence: true
+  validates :age, numericality: { only_integer: true }
+  validates :description, length: { in: 100..500 }
 end
