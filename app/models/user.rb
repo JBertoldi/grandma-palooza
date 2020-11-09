e58d53fe2c61460a17bb89f3e2d6c3f296e1e251
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-09 23:19:41 +0000

    Email validation correct

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   has_many :grandmothers
 
   validates :first_name, :last_name, :age, :location, :email, presence: true
-  validates :email, uniqueness: true, format: { with: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, message: 'Must enter valid email.' }
-  validates :password, presence: true, if: 'id.nil?' 
-  validates :password, length: { minimum: 8 }, unless: "password.nil?"
+  validates :email, uniqueness: true #format: { with: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, message: 'Must enter valid email.' }
+  # validates :password, presence: true, if: 'id.nil?' 
+  # validates :password, length: { minimum: 8 }, unless: "password.nil?"
   validates :age, numericality: { only_integer: true }
 end
