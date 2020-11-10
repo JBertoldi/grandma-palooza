feeb04c5cc4d26777c83cb7ece7c15b2cb5894eb
Oriana Mata <oribouros@gmail.com>
2020-11-10 15:40:54 +0000

    doing the show, edit, update methods on grandma controller plus show page

--- app/controllers/grandmothers_controller.rb
+++ app/controllers/grandmothers_controller.rb
@@ -14,6 +14,20 @@ class GrandmothersController < ApplicationController
     end
   end
 
+  def show
+    @grandmother = Grandmother.find(params[:id])
+  end
+
+  def edit
+    @grandmother = Grandmother.find(params[:id])
+  end
+
+  def update
+    @grandmother.update(grandmother_params)
+
+    redirect_to grandmothers_path(@grandmother), notice: '<strong>Grandma information was updated successfully!</strong>'
+  end
+
   private
 
   def grandmother_params
