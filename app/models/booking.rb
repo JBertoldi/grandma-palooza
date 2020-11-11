ebe8619ec05a1b3d0eb86c2a56f394ee06bda07b
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-11 16:56:19 +0000

    Remove presence key in booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  belongs_to :user, presence: true
-  belongs_to :experience, presence: true
+  belongs_to :user, required: true
+  belongs_to :experience, required: true
 
   validates :date, :price, presence: true
   validates :price, numericality: true
