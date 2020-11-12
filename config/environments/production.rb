f4503e1ea2bbd1f061a2138eece72d95c77f32de
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-12 15:07:15 +0000

    Checking behaviour on the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,16 +20,23 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #!!!!
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   # Added because the images where not showing on heroku
-  config.serve_static_assets = true
-  config.assets.digest = true
+
+
+
+  # ACTIVATE AFTERRRRR
+  # config.serve_static_assets = true
+  # config.assets.digest = true
+
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
