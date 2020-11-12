f20d91b909d5573ddffc5da9cdfaa7bbe938fe3b
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-12 16:32:36 +0000

    Basic structure dashboard create

---
+++ app/javascript/components/toggle_tabs.js
@@ -0,0 +1,10 @@
+const toggleTabs = () => {
+  $("#nav a").click(function (e) {
+    e.preventDefault();
+    $(".toggle").hide();
+    var toShow = $(this).attr('href');
+    $(toShow).show();
+  });
+};
+
+export { toggleTabs };
