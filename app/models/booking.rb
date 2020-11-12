9c1d732b01e5c7e5ace5c04f0f2d9feee49d1d4b
Mathias Leniau <mathiasleniau@gmail.com>
2020-11-12 16:40:06 +0000

    Added dependent destroy to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user, required: true
-  belongs_to :experience, required: true
+  belongs_to :experience, required: true, dependent: :destroy
 
   validates :date, :price, presence: true
   validates :price, numericality: true
