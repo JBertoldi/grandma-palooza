653f5e7d07b69aefdc6e3e6ae308faa3c2da40c1
Mathias Leniau <mathiasleniau@gmail.com>
2020-11-12 16:14:45 +0000

    Updated MVC of experiences

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -43,7 +43,7 @@ class ExperiencesController < ApplicationController
   private
 
   def experience_params
-    params.require(:experience).permit(:title, :description, :price, :duration, :grandmother_id)
+    params.require(:experience).permit(:title, :description, :price,:start_time, :duration, :grandmother_id)
   end
 
   def set_experience
