5ae5ca5787c47e8747334a1cf6cd50aae563a984
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-12 13:47:46 +0000

    Dashboard files create

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  # Added because the images where not showing on heroku
   config.serve_static_assets = true
   config.assets.digest = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
