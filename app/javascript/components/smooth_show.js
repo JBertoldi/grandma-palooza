109d2e82e5780fc5af7518c7063e72a76adc8d77
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-14 23:11:16 +0000

    Add delay to function

--- app/javascript/components/smooth_show.js
+++ app/javascript/components/smooth_show.js
@@ -1,8 +1,8 @@
 const smoothShow = () => {
   $(document).ready(function () {
-    $('#bannerh6').fadeToggle(2000);
-    $('#bannerp').fadeToggle(4000);
-    $('#home-sign').fadeToggle(6000);
+    $('#bannerh6').fadeToggle(2500);
+    $('#bannerp').delay(2000).fadeToggle(2500);
+    $('#home-sign').delay(4000).fadeToggle(2500);
   });
 };
 
